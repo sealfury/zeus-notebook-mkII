1ee3bb711bd25392ffc8da925dc3e11974831770
sealfury <willspecker@gmail.com>
2021-10-06 13:45:58 +0200

    implements transpiling using esbuild functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+  const ref = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const onClick = () => {
-    console.log(input)
+  // Initialize esbuild w/ ref storing service object
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return
+    }
+
+    // result obj will contain a 'code' prop
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    })
+
+    setCode(result.code)
   }
 
   return (
