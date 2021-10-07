475fc7251a4a32ced4f72c42c39b9942b1b0652d
sealfury <willspecker@gmail.com>
2021-10-07 03:57:33 +0200

    filters resolve logic

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,20 +10,21 @@ export const unpkgPathPlugin = (userInput: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // onResolve -> Handle entry file of index.js
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' }
+      })
+
+      // onResolve -> Handle relative module paths beginning with './' or '../'
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+        }
+      })
+
+      // onResolve -> Handle main file of module (ideally)
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args)
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' }
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href,
-          }
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
