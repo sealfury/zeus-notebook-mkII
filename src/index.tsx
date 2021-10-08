955d33c0a6bfafe2195b29f749cc655ec9132b48
sealfury <willspecker@gmail.com>
2021-10-08 13:35:36 +0200

    passes user code to iframe and prevents unwanted escaping

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin, fetchPlugin } from './plugins'
 
 const App = () => {
   const ref = useRef<any>()
+  const iframe = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
   const WASM_URL = 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
@@ -38,13 +39,22 @@ const App = () => {
       },
     })
 
-    setCode(result.outputFiles[0].text)
+    // setCode(result.outputFiles[0].text)
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
 
-  const html = `
-    <script>
-      ${code}
-    </script>
+  const html = /*template*/ `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data)
+          }, false);
+        </script>
+      </body>
+    </html>
   `
 
   return (
@@ -57,7 +67,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox='allow-scripts' srcDoc={html} />
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
     </div>
   )
 }
