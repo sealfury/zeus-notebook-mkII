425f2fb821bff967f37bea254588a50de6d756f1
sealfury <willspecker@gmail.com>
2021-10-12 12:58:35 +0200

    implements horizontal resizer with conditional props

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -15,10 +15,12 @@ const CodeCell = () => {
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue='/* Start Writing Some Code! */'
-          onChange={value => setInput(value)}
-        />
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue='/* Start Writing Some Code! */'
+            onChange={value => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
