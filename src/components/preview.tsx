e9ec77f96f45dfefc5728bc30ea1c23a7a0d482f
sealfury <willspecker@gmail.com>
2021-10-12 14:08:57 +0200

    extracts iframe error handling logic

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -13,15 +13,18 @@ const html = /*template*/ `
       <body>
         <div id="root"></div>
         <script>
+          const handleError = (err) => {
+            // intercept error to display in code preview area
+            const root = document.querySelector('#root')
+            root.innerHTML = 
+              '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err) // output detailed error in console again
+          }
           window.addEventListener('message', (event) => {
             try {
               eval(event.data)
             } catch (err) {
-              // intercept error to display in code preview area
-              const root = document.querySelector('#root')
-              root.innerHTML = 
-                '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err) // output detailed error in console again
+              handleError(err)
             }
           }, false);
         </script>
