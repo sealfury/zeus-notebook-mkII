4a9ece11f8b44841637bf33e8b48fd2f9ae4ac4e
sealfury <willspecker@gmail.com>
2021-10-12 18:54:38 +0200

    implements move/delete cell logic

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -24,9 +24,22 @@ const reducer = produce(
   (state: CellState = initialState, action: Action): CellState | void => {
     switch (action.type) {
       case ActionType.MOVE_CELL:
-        return state
+        const { direction } = action.payload
+        const index = state.order.findIndex(id => id === action.payload.id)
+        const targetIndex = direction === 'up' ? index - 1 : index + 1
+
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return
+        }
+
+        state.order[index] = state.order[targetIndex]
+        state.order[targetIndex] = action.payload.id
+
+        return
       case ActionType.DELETE_CELL:
-        return state
+        delete state.data[action.payload]
+
+        return
       case ActionType.INSERT_CELL_BEFORE:
         return state
       case ActionType.UPDATE_CELL:
