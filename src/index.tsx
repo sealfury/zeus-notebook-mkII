58a88d8824e21be0205ed23e3e740110f632cb37
sealfury <willspecker@gmail.com>
2021-10-12 14:46:39 +0200

    creates text editor component to implement MDEditor

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import 'bulmaswatch/solar/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
 
-import { CodeCell } from './components'
+import { TextEditor } from './components'
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   )
 }
