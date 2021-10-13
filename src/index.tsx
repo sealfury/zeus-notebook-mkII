b7d780f968581d603195a3297100b5ff6e7d0bbb
sealfury <willspecker@gmail.com>
2021-10-13 10:51:35 +0200

    creates cell list / item components to manage all cells

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import { store } from './state'
-import { TextEditor } from './components'
+import { CellList } from './components'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   )
