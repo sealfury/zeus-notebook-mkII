4933a1ff2bd61e3878a44792ad31b4e9eaead5d8
sealfury <willspecker@gmail.com>
2021-10-14 18:12:53 +0200

    specifies command line options

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,10 @@
 import { Command } from 'commander'
 
+// [] = optional value, <> = required value
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]')
   .description('Open a file for editing')
-  .action(() => {
-    console.log('Preparing to serve the file...')
+  .option('-p, --port <number>', 'port to run server', '4005')
+  .action((filename = 'notes.js', options) => {
+    console.log(filename, options)
   })
