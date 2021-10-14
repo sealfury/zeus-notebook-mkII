53b6700db740dbe25ab1d4940bbe49206515d58b
sealfury <willspecker@gmail.com>
2021-10-14 19:06:46 +0200

    scaffolds express for local api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,4 +1,9 @@
+import express from 'express'
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log(`Serving traffic on port ${port}`)
-  console.log(`Saving & Fetching from ${filename} in dir ${dir}`)
+  const app = express()
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
 }
