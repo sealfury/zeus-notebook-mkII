5a99ed25f5da241149ae207d0412bda811464593
sealfury <willspecker@gmail.com>
2021-10-15 10:04:46 +0200

    implements error handling in serve command

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -7,11 +7,27 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server', '4005')
-  .action((filename = 'notes.js', options: { port: string }) => {
-    // calculate directory variable & user-provided path
-    const dir = path.join(process.cwd(), path.dirname(filename))
-    // find actual file name in whatever relative path user provides
-    const fileName = path.basename(filename)
+  .action(async (filename = 'notes.js', options: { port: string }) => {
+    try {
+      // calculate directory variable & user-provided path
+      const dir = path.join(process.cwd(), path.dirname(filename))
 
-    serve(parseInt(options.port), fileName, dir)
+      // find actual file name in whatever relative path user provides
+      const fileName = path.basename(filename)
+
+      await serve(parseInt(options.port), fileName, dir)
+      console.log(
+        `Opened ${filename}! Navigate to http://localhost:${options.port} to edit.`
+      )
+    } catch (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        console.error(
+          `Port ${options.port} is already in use. 
+          Try a different port (hint: use the '-p' flag to select a port number)`
+        )
+      } else {
+        console.log(`Something went wrong: ${err.message}`)
+      }
+      process.exit(1)
+    }
   })
