90e3efd790cd7b7fa9b971f50071f94370f2d64e
sealfury <willspecker@gmail.com>
2021-10-15 10:55:34 +0200

    resolves path to client package

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
+import path from 'path'
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express()
 
-  app.use(
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  )
+  const pkgAbsPath = require.resolve('local-client/build/index.html')
+  app.use(express.static(path.dirname(pkgAbsPath)))
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:3000',
+  //     ws: true,
+  //     logLevel: 'silent',
+  //   })
+  // )
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject)
